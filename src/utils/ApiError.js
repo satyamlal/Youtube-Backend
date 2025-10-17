268ac74163fe310225769364fb6470cf755d6261
Satyam Lal <ssatyamlal@gmail.com>
2025-10-17 23:33:10 +0530

    add ApiError.js, ApiResponse.js, and asyncHandler.js

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,22 +1,22 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message="Something went wrong",
-        errors= [],
-        stack= ""
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.error = errors
-        this.success = false
-        
-        if(stack){
-            this.stack = stack
-        } else {
-            Error.captureStackTrace(this, this.constructor)
-        }
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.error = errors;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export { ApiError }
+export { ApiError };
